afdbd22bf0c1d61c649530783397ec8b6dd0485b
叶博涛 <beating_heart@yeah.net>
2020-05-16 20:37:24 +0800

    提交了更改

---
+++ miniprogram/custom-tab-bar/index.js
@@ -0,0 +1,38 @@
+Component({
+	data: {
+		active: 0,
+		list: [
+			{
+				icon: 'cart-o',
+				text: '点餐',
+				url: '/pages/tab-order/order'
+			},
+			{
+				icon: 'orders-o',
+				text: '订单',
+        url: '/pages/tab-orders/orders'
+			},
+      {
+        icon: 'manager-o',
+        text: '我的',
+        url: '/pages/tab-my/my'
+      }
+		]
+	},
+
+	methods: {
+		onChange(event) {
+			this.setData({ active: event.detail });
+			wx.switchTab({
+				url: this.data.list[event.detail].url
+			});
+		},
+
+		init() {
+			const page = getCurrentPages().pop();
+			this.setData({
+				active: this.data.list.findIndex(item => item.url === `/${page.route}`)
+			});
+		}
+	}
+});
---
+++ miniprogram/pages/tab-orders/orders.js
@@ -0,0 +1,10 @@
+Page({
+  data: {
+
+  },
+
+  //生命周期函数--监听页面显示
+  onShow() {
+    this.getTabBar().init();
+  },
+})
