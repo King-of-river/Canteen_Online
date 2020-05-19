f17383063ef805320d27f390a85a11a1271dc4b2
罗英伟 <1716224950@qq.com>
2020-05-19 18:31:16 +0800

    lyw0519
    初步完成菜品详情页面

--- miniprogram/pages/tab-order/order.js
+++ miniprogram/pages/tab-order/order.js
@@ -3,54 +3,12 @@ const app = getApp()
 Page({
   data: {
     value:"",
-    canteen:"餐厅",
+    canteen:"全部",
     goods:[
       {
         price:"10",
         desc:"套餐描述",
         name:"套餐名称",
-        imageURL: "cloud://test-iqvto.7465-test-iqvto-1302018735/my-image.jpg",
-      },
-      {
-        price: "10",
-        desc: "套餐描述",
-        name: "套餐名称",
-        imageURL: "cloud://test-iqvto.7465-test-iqvto-1302018735/my-image.jpg",
-      },
-      {
-        price: "10",
-        desc: "套餐描述",
-        name: "套餐名称",
-        imageURL: "cloud://test-iqvto.7465-test-iqvto-1302018735/my-image.jpg",
-      },
-      {
-        price: "10",
-        desc: "套餐描述",
-        name: "套餐名称",
-        imageURL: "cloud://test-iqvto.7465-test-iqvto-1302018735/my-image.jpg",
-      },
-      {
-        price: "10",
-        desc: "套餐描述",
-        name: "套餐名称",
-        imageURL: "cloud://test-iqvto.7465-test-iqvto-1302018735/my-image.jpg",
-      },
-      {
-        price: "10",
-        desc: "套餐描述",
-        name: "套餐名称",
-        imageURL: "cloud://test-iqvto.7465-test-iqvto-1302018735/my-image.jpg",
-      },
-      {
-        price: "10",
-        desc: "套餐描述",
-        name: "套餐名称",
-        imageURL: "cloud://test-iqvto.7465-test-iqvto-1302018735/my-image.jpg",
-      },
-      {
-        price: "10",
-        desc: "套餐描述",
-        name: "套餐名称",
         imageURL: "cloud://test-iqvto.7465-test-iqvto-1302018735/my-image.jpg",
       },
       {
