80645a3209dd5088460e0f7da570353974cd7b03
叶博涛 <beating_heart@yeah.net>
2020-05-21 20:46:02 +0800

    提交

--- miniprogram/pages/orderConfirm/orderConfirm.js
+++ miniprogram/pages/orderConfirm/orderConfirm.js
@@ -1,66 +1,29 @@
-// pages/orderConfirm/orderConfirm.js
+import Dialog from '../../dist/dialog/dialog';
+import Toast from '../../dist/toast/toast';
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    show:false
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+ showPopup() {
+    this.setData({ show: true });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onClose() {
+    this.setData({ show: false });
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onSubmit(){
+    Dialog.confirm({
+      title: '订单信息确认',
+      message: '菜品名:\n预计送达时间：\n价格：',
+      messageAlign:"left",
+    })
+      .then(() => {
+        Toast.success("订单确认，跳转到订单页面");
+      })
+      .catch(() => {
+        // on cancel
+      });
   }
 })
