f63fd94b9838dd85a7dbe40dc14c8a5f32ae71e0
叶博涛 <beating_heart@yeah.net>
2020-06-03 18:48:26 +0800

    页面美化
    1. 美化了管理员系列页面
    2. 美化了“我的”页面
    3. 修改了编译模式的名称

--- miniprogram/pages/admin/admin.js
+++ miniprogram/pages/admin/admin.js
@@ -2,32 +2,9 @@ const app = getApp()
 Page({
   data: {
     value: "",
-    canteen: "全部",
-    notice1: [{}],
+
     notice2: [{}],
-    cell: [{}],
-    to_order_assis: [
-      {
-        name: "出餐助手",
-        desc: "分批显示订单，急速出餐",
-      },
-    ],
-    to_statistics: [
-      {
-        name: "订单统计",
-        desc: "查看窗口经营数据",
-      }
-    ],
-    click1(event) {
-      wx.navigateTo({
-        url: '../a-order-assis/a-order-assis',
-      })
-    },
-    tap2(event) {
-      wx.navigateTo({
-        url: '../admin-recipe/admin-recipe',
-      })
-    },
+    
   },
   
 })
