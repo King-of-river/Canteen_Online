896da13fb3bb2d703f028f3cecb0b78d516b167e
王江月 <wangjiangyue864@gmail>
2020-06-04 22:05:41 +0800

    update

--- miniprogram/utils/user_site.js
+++ miniprogram/utils/user_site.js
@@ -1,108 +1,396 @@
-/**
- * 控件当前显示的数据
- * provinces:所有省份
- * citys 选择省对应的所有市,
- * areas选择市对应的所有区
- * areaInfo：点击确定时选择的省市县字符拼接
- * animationAddressMenu：动画
- * addressMenuIsShow：是否可见
- */
-Page({
-  data: {
-   animationAddressMenu: { },
-    addressMenuIsShow: false,
-      value: [0, 0, 0],
-     provinces: [],
-     citys: [],
-     areas: [],
-      areaInfo: ''
-  },
-  onLoad: function () {
-    var address = require('../../utils/city.js')
-    var id = address.provinces[0].id
-    this.setData({
-      provinces: address.provinces,
-      citys: address.citys[id],
-      areas: address.areas[address.citys[id][0].id],
-    })
-  },
-  // 点击所在地区弹出选择框
-  selectDistrict: function (e) {
-    var that = this
-    // 如果已经显示，不在执行显示动画
-    if (that.data.addressMenuIsShow) {
-      return
+var schools = [{
+  "name": "上海理工大学",
+  "id": "010000000"
+}]
+
+var departments = {
+  "010000000": [
+    {
+      "school": "上海理工大学",
+      "name": "第一食堂",
+      "id": "011010000"
+    },
+    {
+      "school": "上海理工大学",
+      "name": "第二食堂",
+      "id": "011020000"
+    },
+    {
+      "school": "上海理工大学",
+      "name": "三公寓一号楼",
+      "id": "010010000"
+    },
+    {
+      "school": "上海理工大学",
+      "name": "三公寓二号楼",
+      "id": "010020000"
+    },
+    {
+      "school": "上海理工大学",
+      "name": "三公寓三号楼",
+      "id": "010030000"
     }
-    // 执行显示动画
-    that.startAddressAnimation(true)
-  },
-  // 执行动画
-  startAddressAnimation: function (isShow) {
-    console.log(isShow)
-    var that = this
-    if (isShow) {
-      // vh是用来表示尺寸的单位，高度全屏是100vh
-      that.animation.translateY(0 + 'vh').step()
-    } else {
-      that.animation.translateY(40 + 'vh').step()
-    }
-    that.setData({
-      animationAddressMenu: that.animation.export(),
-      addressMenuIsShow: isShow,
-    })
-  },
-  // 点击地区选择取消按钮
-  cityCancel: function (e) {
-    this.startAddressAnimation(false)
-  },
-  // 点击地区选择确定按钮
-  citySure: function (e) {
-    var that = this
-    var city = that.data.city
-    var value = that.data.value
-    that.startAddressAnimation(false)
-    // 将选择的城市信息显示到输入框
-    var areaInfo = that.data.provinces[value[0]].name + ',' + that.data.citys[value[1]].name + ',' + that.data.areas[value[2]].name
-    that.setData({
-      areaInfo: areaInfo,
-    })
-  },
-  // 点击蒙版时取消组件的显示
-  hideCitySelected: function (e) {
-    console.log(e)
-    this.startAddressAnimation(false)
-  },
-  // 处理省市县联动逻辑
-  cityChange: function (e) {
-    console.log(e)
-    var value = e.detail.value
-    var provinces = this.data.provinces
-    var citys = this.data.citys
-    var areas = this.data.areas
-    var provinceNum = value[0]
-    var cityNum = value[1]
-    var countyNum = value[2]
-    // 如果省份选择项和之前不一样，表示滑动了省份，此时市默认是省的第一组数据，
-    if (this.data.value[0] != provinceNum) {
-      var id = provinces[provinceNum].id
-      this.setData({
-        value: [provinceNum, 0, 0],
-        citys: address.citys[id],
-        areas: address.areas[address.citys[id][0].id],
-      })
-    } else if (this.data.value[1] != cityNum) {
-      // 滑动选择了第二项数据，即市，此时区显示省市对应的第一组数据
-      var id = citys[cityNum].id
-      this.setData({
-        value: [provinceNum, cityNum, 0],
-        areas: address.areas[citys[cityNum].id],
-      })
-    } else {
-      // 滑动选择了区
-      this.setData({
-        value: [provinceNum, cityNum, countyNum]
-      })
-    }
-    console.log(this.data)
-  },
-})
+  ],
+}
+
+var floors = [{
+  "011010000": [
+    {
+      "departments": "第一食堂",
+      "name": "一楼",
+      "id": "011010100"
+    },
+    {
+      "departments": "第一食堂",
+      "name": "二楼",
+      "id": "011010200"
+    },
+    {
+      "departments": "第一食堂",
+      "name": "三楼",
+      "id": "011010300"
+    },
+  ], 
+  "011020000": [
+    {
+      "departments": "第二食堂",
+      "name": "一楼",
+      "id": "011020100"
+    },
+    {
+      "departments": "第二食堂",
+      "name": "二楼",
+      "id": "011020200"
+    },
+    {
+      "departments": "第二食堂",
+      "name": "三楼",
+      "id": "011020300"
+    },
+  ],
+  "010010000": [
+    {
+      "departments": "三公寓一号楼",
+      "name": "一楼",
+      "id": "010010100"
+    },
+    {
+      "departments": "三公寓一号楼",
+      "name": "二楼",
+      "id": "010010200"
+    },
+    {
+      "departments": "三公寓一号楼",
+      "name": "三楼",
+      "id": "010010300"
+    },
+  ],
+  "010020000": [
+    {
+      "departments": "三公寓二号楼",
+      "name": "一楼",
+      "id": "010020100"
+    },
+    {
+      "departments": "三公寓二号楼",
+      "name": "二楼",
+      "id": "010020200"
+    },
+    {
+      "departments": "三公寓二号楼",
+      "name": "三楼",
+      "id": "010020300"
+    },
+  ],
+  "010030000": [
+    {
+      "departments": "三公寓三号楼",
+      "name": "一楼",
+      "id": "010030100"
+    },
+    {
+      "departments": "三公寓三号楼",
+      "name": "二楼",
+      "id": "010030200"
+    },
+    {
+      "departments": "三公寓三号楼",
+      "name": "三楼",
+      "id": "010030300"
+    },
+  ],
+}]
+
+var numbers = [{
+  "011010100": [
+    {
+      "departments": "一楼",
+      "name": "一号窗口",
+      "id": "011010101"
+    },
+    {
+      "departments": "一楼",
+      "name": "二号窗口",
+      "id": "011010102"
+    },
+    {
+      "departments": "一楼",
+      "name": "三号窗口",
+      "id": "011010103"
+    },
+  ],
+  "011010200": [
+    {
+      "departments": "二楼",
+      "name": "一号窗口",
+      "id": "011010201"
+    },
+    {
+      "departments": "一楼",
+      "name": "二号窗口",
+      "id": "011010202"
+    },
+    {
+      "departments": "一楼",
+      "name": "三号窗口",
+      "id": "011010203"
+    },
+  ], 
+  "011010300": [
+    {
+      "departments": "三楼",
+      "name": "一号窗口",
+      "id": "011010301"
+    },
+    {
+      "departments": "一楼",
+      "name": "二号窗口",
+      "id": "011010302"
+    },
+    {
+      "departments": "一楼",
+      "name": "三号窗口",
+      "id": "011010303"
+    },
+  ], 
+
+  "011020100": [
+    {
+      "departments": "一楼",
+      "name": "一号窗口",
+      "id": "011020101"
+    },
+    {
+      "departments": "一楼",
+      "name": "二号窗口",
+      "id": "011020102"
+    },
+    {
+      "departments": "一楼",
+      "name": "三号窗口",
+      "id": "011020103"
+    },
+  ],
+  "011020200": [
+    {
+      "departments": "二楼",
+      "name": "一号窗口",
+      "id": "011020201"
+    },
+    {
+      "departments": "二楼",
+      "name": "二号窗口",
+      "id": "011020202"
+    },
+    {
+      "departments": "二楼",
+      "name": "三号窗口",
+      "id": "011020203"
+    },
+  ], 
+  "011020300": [
+    {
+      "departments": "一楼",
+      "name": "一号窗口",
+      "id": "011020301"
+    },
+    {
+      "departments": "一楼",
+      "name": "二号窗口",
+      "id": "011020302"
+    },
+    {
+      "departments": "一楼",
+      "name": "三号窗口",
+      "id": "011020303"
+    },
+  ],
+
+  "010010100": [
+    {
+      "departments": "一楼",
+      "name": "01寝室",
+      "id": "010010101"
+    },
+    {
+      "departments": "一楼",
+      "name": "02寝室",
+      "id": "010010102"
+    },
+    {
+      "departments": "一楼",
+      "name": "03寝室",
+      "id": "010010103"
+    },
+  ],
+  "010010200": [
+    {
+      "departments": "二楼",
+      "name": "01寝室",
+      "id": "010010201"
+    },
+    {
+      "departments": "二楼",
+      "name": "02寝室",
+      "id": "010010202"
+    },
+    {
+      "departments": "二楼",
+      "name": "03寝室",
+      "id": "010010203"
+    },
+  ],
+  "010010300": [
+    {
+      "departments": "三楼",
+      "name": "01寝室",
+      "id": "010010301"
+    },
+    {
+      "departments": "三楼",
+      "name": "02寝室",
+      "id": "010010302"
+    },
+    {
+      "departments": "三楼",
+      "name": "03寝室",
+      "id": "010010303"
+    },
+  ],
+
+  "010020100": [
+    {
+      "departments": "一楼",
+      "name": "01寝室",
+      "id": "010020101"
+    },
+    {
+      "departments": "一楼",
+      "name": "02寝室",
+      "id": "010020102"
+    },
+    {
+      "departments": "一楼",
+      "name": "03寝室",
+      "id": "010020103"
+    },
+  ],
+  "010020200": [
+    {
+      "departments": "二楼",
+      "name": "01寝室",
+      "id": "010020201"
+    },
+    {
+      "departments": "二楼",
+      "name": "02寝室",
+      "id": "010020202"
+    },
+    {
+      "departments": "二楼",
+      "name": "03寝室",
+      "id": "010020203"
+    },
+  ],
+  "010020300": [
+    {
+      "departments": "三楼",
+      "name": "01寝室",
+      "id": "010020301"
+    },
+    {
+      "departments": "三楼",
+      "name": "02寝室",
+      "id": "010020302"
+    },
+    {
+      "departments": "三楼",
+      "name": "03寝室",
+      "id": "010020303"
+    },
+  ],
+
+  "010030100": [
+    {
+      "departments": "一楼",
+      "name": "01寝室",
+      "id": "010030101"
+    },
+    {
+      "departments": "一楼",
+      "name": "02寝室",
+      "id": "010030102"
+    },
+    {
+      "departments": "一楼",
+      "name": "03寝室",
+      "id": "010030103"
+    },
+  ],
+  "010030200": [
+    {
+      "departments": "二楼",
+      "name": "01寝室",
+      "id": "010030201"
+    },
+    {
+      "departments": "二楼",
+      "name": "02寝室",
+      "id": "010030202"
+    },
+    {
+      "departments": "二楼",
+      "name": "03寝室",
+      "id": "010030203"
+    },
+  ], 
+  "010030300": [
+    {
+      "departments": "三楼",
+      "name": "01寝室",
+      "id": "010030301"
+    },
+    {
+      "departments": "三楼",
+      "name": "02寝室",
+      "id": "010030302"
+    },
+    {
+      "departments": "三楼",
+      "name": "03寝室",
+      "id": "010030303"
+    },
+  ],
+}]
+
+module.exports = {
+
+  schools, //学校
+
+  departments,//部门 （对学生而言，部门是寝室楼，如三公寓；对管理员来说，部门是食堂，如一食堂）
+
+  floors,     //楼层
+
+  numbers     //号码 （对学生而言，是房间号；对管理者而言是窗口号码）
+
+}
